97f286446b77a9fd19682d7cde5531c95e0b35a4
AnasRoble <Sugule@live.dk>
2023-04-17 16:24:38 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/dtu.project/app/Project.java
+++ src/main/java/dtu.project/app/Project.java
@@ -1,21 +1,32 @@
 package dtu.project.app;
-
+import java.util.ArrayList;
 public class Project {
 
-    private String name;
+    private String title;
     private int budgettedHours;
     private int startWeek;
     private int endWeek;
 
-    public Project(String name, int budgettedHours, int startWeek, int endWeek) {
-        this.name = name;
+    private ArrayList<User> assignedWorkers;
+    private ArrayList<Task> tasks;
+    private String projectManager;
+    private boolean isProjectManagerAssigned;
+    private boolean isCompleted;
+
+    public Project(String title, int budgettedHours, int startWeek, int endWeek) {
+        this.title = title;
         this.budgettedHours = budgettedHours;
         this.startWeek = startWeek;
         this.endWeek = endWeek;
+        this.assignedWorkers = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        this.projectManager = null;
+        this.isProjectManagerAssigned = false;
+        this.isCompleted = false;
     }
 
     public String getName() {
-        return name;
+        return title;
     }
 
     public int getBudgettedHours() {
@@ -29,4 +40,60 @@ public class Project {
     public int getEndWeek() {
         return endWeek;
     }
+
+    public ArrayList<User> getAssignedWorkers() {
+        return assignedWorkers;
+    }
+
+    public void addAssignedWorker(User worker) {
+        assignedWorkers.add(worker);
+    }
+
+    public void removeAssignedWorker(User worker) {
+        assignedWorkers.remove(worker);
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public String getProjectManager() {
+        return projectManager;
+    }
+
+    public void setProjectManager(String projectManager) {
+        this.projectManager = projectManager;
+        this.isProjectManagerAssigned = true;
+    }
+
+    public boolean getIsProjectManagerAssigned() {
+        return isProjectManagerAssigned;
+    }
+
+    public boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    public int getWorkerAmount() {
+        return assignedWorkers.size();
+    }
+
+    public int getTaskAmount() {
+        return tasks.size();
+    }
+
+
+
 }
