23c94ee7589203fcd0fd9f8c429018c17a3ea16d
AnasRoble <Sugule@live.dk>
2023-04-24 16:33:27 +0200

    Merge remote-tracking branch 'origin/main'

--- src/test/java/dtu/project/acceptance_tests/BecomeProjectManager.java
+++ src/test/java/dtu/project/acceptance_tests/BecomeProjectManager.java
@@ -1,29 +1,45 @@
 package dtu.project.acceptance_tests;
 
+import dtu.project.app.Project;
+import dtu.project.app.User;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class BecomeProjectManager {
-    @Given("no project manager is currently assigned to a project")
+    private Project project;
+    private User user;
+
+    @Given("A project is called Project1")
+    public void projectThatIsCalledPoject1() {
+        // create a project with no project manager assigned
+        project = new Project("Project1", 100, 1, 10);
+    }
+
+    @And("A User is called AB")
+    public void userIsCalledAB() {
+        // create a user
+        user = new User("AB");
+    }
+
+    @And("no project manager is currently assigned to a project")
     public void noProjectManagerIsCurrentlyAssignedToAProject() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertFalse(project.getIsProjectManagerAssigned());
     }
-    @Given("the User is currently viewing the project")
-    public void theUserIsCurrentlyViewingTheProject() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Given("the User clicks on the {string} button")
-    public void theUserClicksOnTheButton(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
     @Then("the system will assign the User as the project manager")
     public void theSystemWillAssignTheUserAsTheProjectManager() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // assign the user as the project manager
+        project.setProjectManager(user.getInitials());
+
+        // check if user is assigned as the project manager
+        Assert.assertEquals(user.getInitials(), project.getProjectManager());
     }
-
-
 }
+
+
+
+
+
+
--- use_cases/feature_files/become_project_manager.feature
+++ use_cases/feature_files/become_project_manager.feature
@@ -3,8 +3,8 @@ Description: A worker wants to become a project manager when no project manager 
 Actor: User
 
 Scenario: Worker becomes project manager when no project manager is selected
-Given no project manager is currently assigned to a project
-And the User is logged in
-And the User is currently viewing the project
-And the User clicks on the "Become Project Manager" button
-Then the system will assign the User as the project manager
+
+  Given A project is called Project1
+  And A User is called AB
+  And no project manager is currently assigned to a project
+  Then the system will assign the User as the project manager
