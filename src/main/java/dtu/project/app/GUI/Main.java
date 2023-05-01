28478869a5f30b79ac0a17435127942c8266bb96
jannich <jannichlow@gmail.com>
2023-05-01 13:01:00 +0200

    - Added projectview.fxml - Renamed sample.fxml to login.fxml

--- src/main/java/dtu/project/app/GUI/Main.java
+++ src/main/java/dtu/project/app/GUI/Main.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
-        //URL fxmlLoader = getClass().getResource("sample.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        //URL fxmlLoader = getClass().getResource("login.fxml");
         //System.out.println(fxmlLoader);
-        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../../../resources/sample.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../../../resources/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Project planning tool");
         stage.setScene(scene);
